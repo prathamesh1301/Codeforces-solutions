239c0fc44e88254e43c8c59b53c00f5e5d8d6a39
Prathamesh Autade <57824845+prathamesh1301@users.noreply.github.com>
2021-07-14 22:24:22 +0530

    Create Young Physicist.cpp

---
+++ Young Physicist.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+//const long long m= 1e9+7;
+const int N = 2*(1e5+10);
+#define ll long long
+
+
+void solve(){
+	ll n;
+    cin>>n;
+
+	ll x=0,y=0,z=0;
+	while(n--){
+		ll a,b,c;
+		cin>>a>>b>>c;
+		x+=a;
+		y+=b;
+		z+=c;		
+
+   }
+   if(x==0 && x==y && x==z)
+   		cout<<"YES"<<endl;
+   	else
+   		cout<<"NO"<<endl;
+    
+}
+
+int main(){   
+    //int t;
+    //cin>>t;
+    //while(t--)
+        solve();
+    
+    
+
+    return 0;
+}
+
+
+
+
+	
+	
+
+	
+	
+		
