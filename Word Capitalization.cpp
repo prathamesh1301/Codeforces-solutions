58b211c45769af6d7f718adf0d337a5a217a0329
Prathamesh Autade <57824845+prathamesh1301@users.noreply.github.com>
2021-07-15 21:52:55 +0530

    Create Word Capitalization.cpp

---
+++ Word Capitalization.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+//const long long m= 1e9+7;
+const int N = 2*(1e5+10);
+#define ll long long
+
+void solve(){
+	string s;
+	cin>>s;
+	if(!isupper(s[0]))
+		s[0]=toupper(s[0]);
+	cout<<s<<endl;
+}
+
+int main(){   
+    //int t;
+    //cin>>t;
+    //while(t--)
+        solve();
+    
+    
+
+    return 0;
+}
