6f81d0a672758a1260922f1c441f9bcc9c774230
Prathamesh Autade <57824845+prathamesh1301@users.noreply.github.com>
2022-02-14 23:34:06 +0530

    Create Same Differences.cpp

---
+++ Same Differences.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld double
+int M=1e9+7;
+
+const int array_size = 1e6+100;
+bool primes[array_size];
+
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+ll lcm(ll a, ll b){return ((a/gcd(a,b))*b);}
+void sieve(){
+  
+   primes[0] = false;
+   primes[1] = false;
+   for(ll i = 2;i*i<=array_size;i++){
+      if(primes[i]){
+         for(ll j=i*i;j<=array_size;j+=i)
+            primes[j] = false;
+      }
+   }
+}
+
+void solve(){
+    int n;
+  cin >> n;
+  map<int, int> a;
+  long long res = 0;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    x -= i;
+    res += a[x];
+    a[x]++;
+  }
+  cout << res << endl;
+    
+
+    
+}
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); 
+    //memset(primes,true,sizeof(primes));
+    //sieve();  
+    //memset(dp,false,sizeof(dp));
+    //precal();
+    int t;
+
+    cin>>t;
+
+    while(t--)
+        solve();
+    return 0;
+
+
+}
